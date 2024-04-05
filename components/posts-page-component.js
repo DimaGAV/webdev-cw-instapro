aecb0ba40a31d5d8c70b52aaa9f2703543f3229a
DimaGAV <gav_gav82@mail.ru>
2024-04-05 21:01:30 +0300

    после консультации

--- components/posts-page-component.js
+++ components/posts-page-component.js
@@ -2,7 +2,7 @@ import { POSTS_PAGE, USER_POSTS_PAGE } from "../routes.js";
 import { renderHeaderComponent } from "./header-component.js";
 import {
   posts,
-  page,
+  user,
   goToPage,
   getToken,
   setPosts,
@@ -20,6 +20,11 @@ export function renderPostsPageComponent({ appEl }) {
    */
   const postsHtml = posts
     .map((post, index) => {
+      // const isLiked = post.likes.find((like) => {
+      //   return like.id === user._id
+      // })
+      const isLiked = isLikedPost(post)
+      // debugger
       return `
     <li data-index="${index}" class="post">
                     <div class="post-header" data-user-id="${post.user.id}">
@@ -36,7 +41,7 @@ export function renderPostsPageComponent({ appEl }) {
         post.isLiked
       }" data-index="${index}" class="like-button">
                       <img src="${
-                        posts[index].isLiked
+                        isLiked
                           ? `./assets/images/like-active.svg`
                           : `./assets/images/like-not-active.svg`
                       }">
@@ -65,7 +70,7 @@ export function renderPostsPageComponent({ appEl }) {
 
   const appHtml = `
               <div class="page-container">
-                <div class="header-container"></div>
+                <header class="header-container"></header>
                 <ul class="posts">${postsHtml}</ul>
               </div>`;
 
@@ -90,17 +95,22 @@ export function renderPostsPageComponent({ appEl }) {
       event.stopPropagation();
       const postId = likeButton.dataset.postId;
       const index = likeButton.dataset.index;
-      const isActive = posts[index].isLiked;
+      // const isActive = isLiked;
+      // const isLiked = posts[index].likes.find((like) => {
+      //   return like.id === user._id
+      // })
+      const isLiked = isLikedPost(posts[index])
 
-      if (isActive) {
+      if (isLiked) {
         return onDisLikeClick({ token: getToken(), id: postId }).then((res) => {
           const updatedPost = res.post; // Получаем обновленный пост из ответа
-          console.log(updatedPost);
+          // console.log(updatedPost);
           const newPosts = posts.map((post) =>
             post.id === updatedPost.id ? updatedPost : post
           ); // Обновляем пост в массиве posts
           setPosts(newPosts);
-          console.log(newPosts);
+          
+          // console.log(newPosts);
           renderPostsPageComponent({
             appEl,
           });
@@ -114,6 +124,7 @@ export function renderPostsPageComponent({ appEl }) {
           );
           setPosts(newPosts);
           console.log(newPosts);
+          // debugger
           renderPostsPageComponent({
             appEl,
           });
@@ -123,3 +134,9 @@ export function renderPostsPageComponent({ appEl }) {
     
   }
 }
+
+function isLikedPost(post) {
+  return post.likes.find((like) => {
+    return like.id === user._id
+  })
+} 
